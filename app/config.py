92bf8cae778c3a772907dbd32b9cd6773bfd8960
fauwaz-911 <60237483+fauwaz-911@users.noreply.github.com>
2026-03-03 17:41:42 +0100

    Update config.py
    used newly generated config.py

--- app/config.py
+++ app/config.py
@@ -66,9 +66,9 @@ def load_settings() -> Settings:
 
     return Settings(
         WORKSPACE_ID               = os.getenv("WORKSPACE_ID", "default"),
-        NOTION_TOKEN               = os.environ["NOTION_API_KEY"],
-        NOTION_DB_ID               = os.environ["NOTION_TASKS_DB_ID"],
-        TELEGRAM_TOKEN             = os.environ["TELEGRAM_BOT_TOKEN"],
+        NOTION_API_KEY             = os.environ["NOTION_API_KEY"],
+        NOTION_TASKS_DB_ID         = os.environ["NOTION_TASKS_DB_ID"],
+        TELEGRAM_BOT_TOKEN         = os.environ["TELEGRAM_BOT_TOKEN"],
         TELEGRAM_CHAT_ID           = os.environ["TELEGRAM_CHAT_ID"],
         MORNING_HOUR               = int(os.getenv("MORNING_HOUR", "10")),
         MORNING_MINUTE             = int(os.getenv("MORNING_MINUTE", "0")),
